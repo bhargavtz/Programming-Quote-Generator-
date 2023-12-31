54312e008307d6930c7671314c6d96f1145bd8f3
bhargavtz <bhargav05@yandex.com>
2023-12-31 19:11:30 +0530

    v.1.3
    update JavaScript & copy bottom

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// Getting the quote container and button from the DOM
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const newQuoteButton = document.getElementById('new-quote');
+const copyButton = document.getElementById('copy-quote');
+
 // Fetching the quotes from the JSON file
 fetch('quotes.json')
     .then(response => response.json())
     .then(quotes => {
-        // Getting the quote container and button from the DOM
-        const quoteContainer = document.getElementById('quote-container');
-        const quoteText = document.getElementById('quote');
-        const newQuoteButton = document.getElementById('new-quote');
-
         // Function to generate a random quote
         function generateQuote() {
             const randomIndex = Math.floor(Math.random() * quotes.length);
@@ -20,9 +21,23 @@ fetch('quotes.json')
 
         // Generate a new quote when the button is clicked
         newQuoteButton.addEventListener('click', generateQuote);
+
+        // Copy the quote text when the button is clicked
+        copyButton.addEventListener('click', () => {
+            const textArea = document.createElement('textarea');
+            textArea.value = quoteText.textContent;
+            document.body.appendChild(textArea);
+            textArea.select();
+            document.execCommand('copy');
+            document.body.removeChild(textArea);
+
+            copyButton.innerText = 'Copied!';
+            setTimeout(() => {
+                copyButton.innerText = 'Copy Quote';
+            }, 2000);
+        });
     })
     .catch(error => {
         console.error('Error:', error);
         quoteContainer.textContent = 'Failed to load quotes.';
     });
-    const copyButtons = document.querySelectorAll('.copy-button');
